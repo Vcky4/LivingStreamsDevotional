6416a78d044972701824d9d6ba02579d50e2781f
vicksoson <okon.victor.u@gmail.com>
2022-02-08 17:39:13 +0100

    feet - devotional body and nugget

--- app/src/main/java/com/ghor/livingstreamsdevotional/ui/devotional/DevotionalViewModel.kt
+++ app/src/main/java/com/ghor/livingstreamsdevotional/ui/devotional/DevotionalViewModel.kt
@@ -9,11 +9,15 @@ class DevotionalViewModel : ViewModel() {
     private val _qualifierText = MutableLiveData<String>("Light House Fellowship Edition")
     private val _dateText = MutableLiveData<String>("Saturday 01 May")
     private val _topicText = MutableLiveData<String>("wisdom will bring success")
+    private val _bodyText = MutableLiveData<String>("wisdom will bring success")
+    private val _nuggetText = MutableLiveData<String>("wisdom will bring success")
 
     //public
     val qualifierText: LiveData<String> = _qualifierText
     val dateText: LiveData<String> = _dateText
     val topicText: LiveData<String> = _topicText
+    val bodyText: LiveData<String> = _bodyText
+    val nuggetText: LiveData<String> = _nuggetText
 
     //methods
 
