f8a0434ce5468dc4a25e32582741fe6bfa8f31b5
vicksoson <okon.victor.u@gmail.com>
2022-02-12 10:38:47 +0100

    fix - admin devotional

--- app/src/main/java/com/ghor/livingstreamsdevotional/ui/admindevotional/DevotionalAdminFragment.kt
+++ app/src/main/java/com/ghor/livingstreamsdevotional/ui/admindevotional/DevotionalAdminFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.ghor.livingstreamsdevotional.databinding.FragmentAdminDevotionalBinding
 
@@ -27,13 +25,8 @@ private var _binding: FragmentAdminDevotionalBinding? = null
       ViewModelProvider(this)[DevotionalAdminViewModel::class.java]
 
     _binding = FragmentAdminDevotionalBinding.inflate(inflater, container, false)
-    val root: View = binding.root
 
-    val textView: TextView = binding.textDashboard
-    devotionalAdminViewModel.text.observe(viewLifecycleOwner, {
-      textView.text = it
-    })
-    return root
+    return binding.root
   }
 
 override fun onDestroyView() {
