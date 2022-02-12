3ce68ade1d43f664cf47880461dd947c0d0747a7
vicksoson <okon.victor.u@gmail.com>
2022-02-12 18:41:14 +0100

    feet - admin nugget

--- app/src/main/java/com/ghor/livingstreamsdevotional/ui/adminnugget/NuggetAdminFragment.kt
+++ app/src/main/java/com/ghor/livingstreamsdevotional/ui/adminnugget/NuggetAdminFragment.kt
@@ -1,12 +1,18 @@
 package com.ghor.livingstreamsdevotional.ui.adminnugget
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ghor.livingstreamsdevotional.databinding.FragmentAdminNuggetBinding
+import com.ghor.livingstreamsdevotional.ui.adminauthentication.Utility
+import com.ghor.livingstreamsdevotional.ui.nuggets.NuggetAdapter
 
 class NuggetAdminFragment : Fragment() {
 
@@ -26,11 +32,66 @@ private var _binding: FragmentAdminNuggetBinding? = null
 
     _binding = FragmentAdminNuggetBinding.inflate(inflater, container, false)
 
-
+    handleClicks()
+    nuggetTextWatchers()
     return binding.root
   }
 
-override fun onDestroyView() {
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    val adapter = NuggetAdapter()
+    binding.nuggetRecycler.layoutManager = LinearLayoutManager(activity)
+    binding.nuggetRecycler.adapter = adapter
+
+    nuggetAdminViewModel.nuggets.observe(this,{
+      adapter.setUpNuggets(it)
+    })
+
+
+  }
+
+  private fun handleClicks() {
+
+    //on click of login button
+    binding.sendBt.setOnClickListener {
+
+      //here you can check for network availability first, if the network is available, continue
+      if (Utility.isNetworkAvailable(context)) {
+
+        nuggetAdminViewModel.addNugget(binding.nuggetText.text.toString())
+
+      } else {
+
+        Toast.makeText(context, "Please check your internet", Toast.LENGTH_LONG).show()
+
+      }
+
+    }
+  }
+
+  private fun nuggetTextWatchers() {
+    val watcher: TextWatcher = object : TextWatcher {
+      override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+      override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        val s1 = binding.nuggetText.text.toString()
+
+        //check if they are empty, make button not clickable
+        binding.sendBt.isEnabled = s1.isNotEmpty()
+
+      }
+
+
+      override fun afterTextChanged(s: Editable?) {}
+
+    }
+    binding.nuggetText.addTextChangedListener(watcher)
+  }
+
+
+
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
--- app/src/main/java/com/ghor/livingstreamsdevotional/ui/adminnugget/NuggetAdminViewModel.kt
+++ app/src/main/java/com/ghor/livingstreamsdevotional/ui/adminnugget/NuggetAdminViewModel.kt
@@ -3,11 +3,24 @@ package com.ghor.livingstreamsdevotional.ui.adminnugget
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ghor.livingstreamsdevotional.ui.nuggets.NuggetData
 
 class NuggetAdminViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+
+    private val _nuggets = MutableLiveData<List<NuggetData>>()
+
+    private val nuggetsList =  mutableListOf<NuggetData>(
+        NuggetData("God is love and He loves you no matter what"),
+        NuggetData("All things works together for good to all those that love God"),
+        NuggetData("You can succeed doing nothing, you must work it!"),
+        NuggetData("There is nothing God can not solve"),
+        NuggetData("God is love and He loves you no matter what"),
+    )
+    val nuggets: LiveData<List<NuggetData>> = _nuggets
+
+    fun addNugget(nugget: String){
+        nuggetsList.add(NuggetData(nugget))
+        _nuggets.value = nuggetsList
     }
-    val text: LiveData<String> = _text
 }
