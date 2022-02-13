19d12150cf8c6dcda610c835bac4cef2058b8bfd
vicksoson <okon.victor.u@gmail.com>
2022-02-13 01:26:04 +0100

    fix

--- app/src/main/java/com/ghor/livingstreamsdevotional/ui/adminnugget/NuggetAdminViewModel.kt
+++ app/src/main/java/com/ghor/livingstreamsdevotional/ui/adminnugget/NuggetAdminViewModel.kt
@@ -4,29 +4,25 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ghor.livingstreamsdevotional.ui.nuggets.NuggetData
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class NuggetAdminViewModel : ViewModel() {
 
     private val database: DatabaseReference = Firebase.database.reference
 
-    private val nuggetsList =  mutableListOf<NuggetData>(
-        NuggetData("God is love and He loves you no matter what"),
-        NuggetData("All things works together for good to all those that love God"),
-        NuggetData("You can succeed doing nothing, you must work it!"),
-        NuggetData("There is nothing God can not solve"),
-        NuggetData("God is love and He loves you no matter what"),
-    )
-    private val _nuggets = MutableLiveData<List<NuggetData>>(nuggetsList)
-
+    private val nuggetsList =  mutableListOf<NuggetData>()
+    private val _nuggets = MutableLiveData<List<NuggetData>>()
 
     val nuggets: LiveData<List<NuggetData>> = _nuggets
 
     fun addNugget(nugget: String){
         nuggetsList.add(NuggetData(nugget))
+        database.child("nuggets").setValue(nuggetsList)
     }
 
-    fun writeNugget(nugget: String){
-        database.child(nuggets).setValue(nugget)
+    fun readNugget(){
+
     }
 }
